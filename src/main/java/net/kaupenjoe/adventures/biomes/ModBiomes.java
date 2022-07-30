0e2431e338c1382fa93b2a536fe4b90c0471b326
NexusDino <86781290+Nexus-Dino@users.noreply.github.com>
2022-07-30 12:18:35 +0100

    add new version of RS, dimensions, biome fix and fog modification

--- src/main/java/net/kaupenjoe/adventures/biomes/ModBiomes.java
+++ src/main/java/net/kaupenjoe/adventures/biomes/ModBiomes.java
@@ -1,18 +1,21 @@
 package net.kaupenjoe.adventures.biomes;
 
 import net.kaupenjoe.adventures.Adventures;
+import net.kaupenjoe.resourceslimes.block.ModBlocks;
 import net.minecraft.core.Registry;
+import net.minecraft.data.worldgen.BiomeDefaultFeatures;
+import net.minecraft.data.worldgen.placement.EndPlacements;
 import net.minecraft.data.worldgen.placement.VegetationPlacements;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.util.VisibleForDebug;
-import net.minecraft.world.level.biome.Biome;
-import net.minecraft.world.level.biome.BiomeGenerationSettings;
+import net.minecraft.world.level.biome.*;
 import net.minecraft.world.level.levelgen.GenerationStep;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
+import org.jetbrains.annotations.VisibleForTesting;
 
 public class ModBiomes {
     public static final DeferredRegister<Biome> BIOMES =
@@ -27,11 +30,16 @@ public class ModBiomes {
         return BIOMES.register(key.location().getPath(), () -> biome);
     }
 
-    @VisibleForDebug
-    private static Biome createSlimeWastelands() {
-        BiomeGenerationSettings.Builder builder = new BiomeGenerationSettings.Builder();
-        builder.addFeature(GenerationStep.Decoration.VEGETAL_DECORATION, VegetationPlacements.TREES_SWAMP);
-        return (new Biome.BiomeBuilder()).precipitation(Biome.Precipitation.RAIN).build();
+    private static Biome baseEndBiome(BiomeGenerationSettings.Builder p_194825_) {
+        MobSpawnSettings.Builder mobspawnsettings$builder = new MobSpawnSettings.Builder();
+        BiomeDefaultFeatures.endSpawns(mobspawnsettings$builder);
+        return (new Biome.BiomeBuilder()).precipitation(Biome.Precipitation.NONE).temperature(0.5F).downfall(0.5F).specialEffects((new BiomeSpecialEffects.Builder()).waterColor(4159204).waterFogColor(329011).fogColor(10518688).skyColor(0).ambientMoodSound(AmbientMoodSettings.LEGACY_CAVE_SETTINGS).build()).mobSpawnSettings(mobspawnsettings$builder.build()).generationSettings(p_194825_.build()).build();
+    }
+
+    @VisibleForTesting
+    public static Biome createSlimeWastelands() {
+        BiomeGenerationSettings.Builder biomegenerationsettings$builder = (new BiomeGenerationSettings.Builder()).addFeature(GenerationStep.Decoration.SURFACE_STRUCTURES, EndPlacements.END_GATEWAY_RETURN).addFeature(GenerationStep.Decoration.VEGETAL_DECORATION, EndPlacements.CHORUS_PLANT);
+        return baseEndBiome(biomegenerationsettings$builder);
     }
 
     public static void register(IEventBus eventBus) {
