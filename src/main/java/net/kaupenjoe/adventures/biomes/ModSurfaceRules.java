9d8f0c27123400140c79094a3693416bb86eb44a
NexusDino <86781290+Nexus-Dino@users.noreply.github.com>
2022-07-30 12:22:19 +0100

    use slimey dirt as a custom biome surface

--- src/main/java/net/kaupenjoe/adventures/biomes/ModSurfaceRules.java
+++ src/main/java/net/kaupenjoe/adventures/biomes/ModSurfaceRules.java
@@ -1,13 +1,13 @@
 package net.kaupenjoe.adventures.biomes;
 
 import net.kaupenjoe.adventures.mixin.SurfaceRulesAccessor;
-import net.minecraft.world.level.block.Blocks;
+import net.kaupenjoe.resourceslimes.block.ModBlocks;
 import net.minecraft.world.level.levelgen.SurfaceRules;
 
 public class ModSurfaceRules {
-    public static final SurfaceRules.RuleSource SWAMP_DIRT = SurfaceRulesAccessor.makeBlockStateRule(Blocks.DIRT);
+    public static final SurfaceRules.RuleSource SLIMEY_DIRT = SurfaceRulesAccessor.makeBlockStateRule(ModBlocks.SLIMEY_DIRT.get());
 
     public static SurfaceRules.RuleSource createSlimeWastelands() {
-        return SurfaceRules.sequence(SurfaceRules.ifTrue(SurfaceRules.isBiome(ModBiomes.SLIME_WASTELANDS_KEY), SWAMP_DIRT));
+        return SurfaceRules.sequence(SurfaceRules.ifTrue(SurfaceRules.isBiome(ModBiomes.SLIME_WASTELANDS_KEY), SLIMEY_DIRT));
     }
 }
